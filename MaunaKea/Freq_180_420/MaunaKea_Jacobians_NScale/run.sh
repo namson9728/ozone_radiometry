aab55c3dad380fb12487b44d546696f5920d534d
Namson Nguyen <nnguyen28@worcester.edu>
2024-11-16 15:57:41 -0500

    Updated MaunaKea plots

---
+++ MaunaKea/Freq_180_420/MaunaKea_Jacobians_NScale/run.sh
@@ -0,0 +1,7 @@
+for Trop_NSCALE in 0.206 0.513 1.00 2.07 4.60; do
+    echo "Starting loop for NSCALE=$Trop_NSCALE"
+    am MaunaKea_SON_50.amc \
+        180 GHz 420 GHz 1 MHz 0 deg 277 K $Trop_NSCALE \
+        > trop_h2o_Jacobian_${Trop_NSCALE}.out \
+        2> trop_h2o_Jacobian_${Trop_NSCALE}.err
+done
---
+++ MaunaKea/Freq_180_420/MaunaKea_Jacobians_ZA/run.sh
@@ -0,0 +1,7 @@
+for ZENITH_ANGLE in 0 45 60 70; do
+    echo "Starting loop for ZENITH=$ZENITH_ANGLE"
+    am MaunaKea_SON_50.amc \
+        180 GHz 420 GHz 1 MHz $ZENITH_ANGLE deg 277 K 1.0 \
+        > trop_h2o_Jacobian_${ZENITH_ANGLE}.out \
+        2> trop_h2o_Jacobian_${ZENITH_ANGLE}.err
+done

bb9944c08e5088f933958cb0102b7e315782edda
Namson Nguyen <nnguyen28@worcester.edu>
2024-12-05 18:04:09 -0500

    interpolation code

---
+++ MaunaKea/interpolation/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+        am MaunaKea_SON_50.amc             180 GHz 420 GHz 1 MHz 75.0 deg 277 K 3.0             > MaunaKea_Tb_Spectrum_75.0_3.0.out             2> MaunaKea_Tb_Spectrum_75.0_3.0.err
+        
